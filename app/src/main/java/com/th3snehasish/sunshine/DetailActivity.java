ac0261016f35be7457787849d3dcfe6f4d26b755
th3snehasish <sn3hasishnayak@gmail.com>
2016-02-20 18:36:07 +0530

    Removed Floating Action Buttons and unused codes from DetailsActivity.

--- app/src/main/java/com/th3snehasish/sunshine/DetailActivity.java
+++ app/src/main/java/com/th3snehasish/sunshine/DetailActivity.java
@@ -2,15 +2,10 @@ package com.th3snehasish.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
-//import android.widget.ShareActionProvider;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,15 +15,6 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
